00df1049f4b8683af775e91a6eb4b3eb38b4b1ef
Youssef Mahmoud <youssef.shubra@yahoo.com>
2023-10-07 18:35:01 +0300

    Building game cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import {CanceledError} from "axios";
 
-interface Game {
+//use custom hook to seperate logic
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
 }
   
 interface FetchGamesResponse {
