de38b59dc4b607fcd66992a6d309ea640ced0f09
Youssef Mahmoud <youssef.shubra@yahoo.com>
2023-10-08 15:34:57 +0300

    Hide side bar if we got error from the server

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -15,10 +15,11 @@ interface Props {
 }
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
-  const { data, isLoading } = useGenres();
+  const { data, isLoading, error } = useGenres();
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
   ];
+  if (error) return null;
   return (
     <List>
       {isLoading &&
